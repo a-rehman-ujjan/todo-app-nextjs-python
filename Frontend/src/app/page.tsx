8eb684c729e66d92a936f5a19479634c6f2dd9b8
Abdul Rehman Ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2024-04-09 01:07:37 +0500

    Update page.tsx
    Changed some styling

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,13 +1,14 @@
 import { Modal } from "@/components/ui/Modal";
 import TodoTable from "@/components/TodoTable";
 import { Button } from "@/components/ui/button";
+import { FaPlus } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="flex min-h-screen flex-col p-24 bg-[#050515]">
       <section className="flex justify-center items-center">
         <Modal title="Add Task" Add={true}>
-        <Button variant="outline" className="bg-emerald-400 w-48 text-white font-semibold px-2 py-1 capitalize text-lg">Add New Task</Button>
+        <Button variant="default" className="bg-[#034aa6] w-48 text-white font-semibold px-2 py-1 capitalize text-pretty">Add New Task &nbsp;<FaPlus/></Button>
         </Modal>
       </section>
       <section className="flex justify-center items-center mt-8">
